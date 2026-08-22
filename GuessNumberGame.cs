061a31fe8f681dfebe7dbe10be865de12f5b136a
Bober <bhriguanovskij@gmail.com>
2026-08-22 09:09:24 +0200

    adding settings

--- GuessNumberGame.cs
+++ GuessNumberGame.cs
@@ -1,91 +1,186 @@
-﻿using System;
+﻿
+using System;
 
 public class GuessNumberGame
 {
-    int[] easy = { 1, 100 };
-    int[] medium = { 1, 1000 };
-    int[] hard = { 1, 10000 };
+    private readonly int[] easy = { 1, 100 };
+    private readonly int[] medium = { 1, 1000 };
+    private readonly int[] hard = { 1, 10000 };
 
-    Random random = new Random();
+    private readonly Random random = new Random();
 
-    int RandomNumber(int min, int max)
+    private int RandomNumber(int min, int max)
     {
-        max++;
-        return random.Next(min, max);
+        return random.Next(min, max + 1);
     }
 
     public void Run()
     {
-        ConsolePrinter.PrintText("Hi! This is the \"Guess the Number\" game. Ready to start?");
-        ConsolePrinter.PrintText("Press the space to start.");
-
         while (true)
         {
+            ConsolePrinter.PrintText(
+                "Hi! This is the \"Guess the Number\" game. Ready to start?"
+            );
+
+            ConsolePrinter.PrintText("0 exit 1 start 2 settings");
+
             ConsoleKeyInfo key = Console.ReadKey(true);
 
-            if (key.Key == ConsoleKey.Spacebar)
+            switch (key.Key)
             {
-                break;
+                case ConsoleKey.D0:
+                    Environment.Exit(0);
+                    break;
+
+                case ConsoleKey.D1:
+                    StartGame();
+                    break;
+
+                case ConsoleKey.D2:
+                    OpenSettings();
+                    break;
+
+                default:
+                    ConsolePrinter.PrintText("Invalid option. Please try again.");
+                    break;
             }
         }
+    }
 
-        ConsolePrinter.PrintText("Choose a difficulty level: 1 (easy) to 3 (hard).");
+    private void StartGame()
+    {
+        ConsolePrinter.PrintText(
+            "Choose difficulty: 1 easy 2 medium 3 hard"
+        );
 
-        int difficultyLevel = ConsolePrinter.ReadInt("Enter a number from 1 to 3.", 1, 3);
+        int difficulty = ConsolePrinter.ReadInt(
+            "Enter difficulty (1-3): ",
+            1,
+            3
+        );
 
-        ConsolePrinter.PrintText($"You chose difficulty level {difficultyLevel}.");
-        int[] selectedRange;
+        int min;
+        int max;
 
-        if (difficultyLevel == 1)
+        switch (difficulty)
         {
-            selectedRange = easy;
-            ConsolePrinter.PrintText($"You chose Easy. I picked a number between {easy[0]} and {easy[1]}.");
-        }
-        else if (difficultyLevel == 2)
-        {
-            selectedRange = medium;
-            ConsolePrinter.PrintText($"You chose Medium. I picked a number between {medium[0]} and {medium[1]}.");
-        }
-        else
-        {
-            selectedRange = hard;
-            ConsolePrinter.PrintText($"You chose Hard. I picked a number between {hard[0]} and {hard[1]}.");
+            case 1:
+                min = easy[0];
+                max = easy[1];
+                break;
+
+            case 2:
+                min = medium[0];
+                max = medium[1];
+                break;
+
+            case 3:
+                min = hard[0];
+                max = hard[1];
+                break;
+
+            default:
+                return;
         }
 
-        int secretNumber = RandomNumber(selectedRange[0], selectedRange[1]);
-
-        ConsolePrinter.PrintText("ok, let's start the game!");
-        Console.Clear();
-
+        int numberToGuess = RandomNumber(min, max);
         int attempts = 0;
-        string[] wrongComments = { "Nope!", "Try again...", "Wrong!", "Not even close 😴", "Nah.", "Keep trying..." };
+
+        ConsolePrinter.PrintText(
+            $"I have chosen a number between {min} and {max}. Try to guess it!"
+        );
 
         while (true)
         {
-            string? input = Console.ReadLine();
-            int guess;
-
-            if (!int.TryParse(input, out guess))
-            {
-                ConsolePrinter.PrintText("Enter a valid number!");
-                continue;
-            }
+            int playerGuess = ConsolePrinter.ReadInt(
+                $"Enter your guess ({min}-{max}): ",
+                min,
+                max
+            );
 
             attempts++;
 
-            if (guess < secretNumber)
+            if (playerGuess < numberToGuess)
             {
-                ConsolePrinter.PrintText($"{wrongComments[attempts % wrongComments.Length]} Too low!");
+                ConsolePrinter.PrintText("Too low! Try again.");
             }
-            else if (guess > secretNumber)
+            else if (playerGuess > numberToGuess)
             {
-                ConsolePrinter.PrintText($"{wrongComments[attempts % wrongComments.Length]} Too high!");
+                ConsolePrinter.PrintText("Too high! Try again.");
             }
             else
             {
-                ConsolePrinter.PrintText($"Correct! The number was {secretNumber}.");
-                ConsolePrinter.PrintText($"You guessed it in {attempts} attempts!");
+                ConsolePrinter.PrintText(
+                    $"Congratulations! You've guessed the number {numberToGuess} in {attempts} attempts."
+                );
+
                 break;
+            }
+        }
+    }
+
+    private void OpenSettings()
+    {
+        while (true)
+        {
+            ConsolePrinter.PrintText(
+                "1 Set letter speed 2 Set comma delay 3 Set sentence end delay 4 Exit"
+            );
+
+            ConsoleKeyInfo settingsKey = Console.ReadKey(true);
+
+            switch (settingsKey.Key)
+            {
+                case ConsoleKey.D1:
+                    int newSpeed = ConsolePrinter.ReadInt(
+                        "Enter new letter speed (ms): ",
+                        0,
+                        1000
+                    );
+
+                    ConsolePrinter.LetterSpeed = newSpeed;
+
+                    ConsolePrinter.PrintText(
+                        $"Letter speed set to {newSpeed} ms."
+                    );
+                    break;
+
+                case ConsoleKey.D2:
+                    int newDelay = ConsolePrinter.ReadInt(
+                        "Enter new comma delay (ms): ",
+                        0,
+                        1000
+                    );
+
+                    ConsolePrinter.CommaDelay = newDelay;
+
+                    ConsolePrinter.PrintText(
+                        $"Comma delay set to {newDelay} ms."
+                    );
+                    break;
+
+                case ConsoleKey.D3:
+                    int newEndDelay = ConsolePrinter.ReadInt(
+                        "Enter new sentence end delay (ms): ",
+                        0,
+                        1000
+                    );
+
+                    ConsolePrinter.SentenceEndDelay = newEndDelay;
+
+                    ConsolePrinter.PrintText(
+                        $"Sentence end delay set to {newEndDelay} ms."
+                    );
+                    break;
+
+                case ConsoleKey.D4:
+                    return;
+
+                default:
+                    ConsolePrinter.PrintText(
+                        "Invalid option. Please try again."
+                    );
+                    break;
             }
         }
     }
